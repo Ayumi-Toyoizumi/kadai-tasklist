fba1c1998d55c3c0f83f572b10805759a81b4c59
Ayumi.Toyoizumi <Ayumi_Toyoizumi@sis2.saison.co.jp>
2023-07-14 20:09:44 +0900

    コミット

---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,29 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Tasks;
+
+public class MessageValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Tasks m) {
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = validateTitle(m.getContent());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        return errors;
+    }
+
+    // タイトルの必須入力チェック
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タスクを入力してください。";
+        }
+
+        return "";
+    }
+}
